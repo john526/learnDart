55faf13088a28ecda58b33a1aa18984d592127f8
Tiaha <tiahadoua@gmail.com>
2020-01-15 02:19:35 +0000

    POO

--- POO/object.dart
+++ POO/object.dart
@@ -49,6 +49,18 @@ void main(){
 
   var car = Car(color: "green",km: 254,marque: "Fiat",modele: "Punto",years: 2019);
   print(car.marque);
+
+
+  // or
+
+  var lion= Animal("Lion","meat",15,50);
+  print(lion.age);
+  print(lion.name);
+
+  var termite = Insect("termite","sheet",5,54,54.2,"forest");
+  print(termite.home);
+  print(termite.fly);
+  termite.eat();
 }
 
 class Car{
@@ -69,5 +81,41 @@ class Car{
   void run(){
     km++;
     print("brom brom");
+  }
+}
+
+
+// OR
+
+class Animal{
+  String name;
+  String food;
+  int age;
+  double run;
+
+
+  Animal(String name, String food,int age, double run){
+    this.name = name;
+    this.age = age;
+    this.food = food;
+    this.run = run;
+  }
+
+  void eat (){
+    var i =0;
+    print("the value of : $name");
+    i++;
+    print(i);
+  }
+}
+
+class Insect extends Animal{
+
+  double fly;
+  String home;
+
+  Insect(String name,String food,int age, double run,double fly,String home):super(name,food,age,run){
+    this.fly = fly;
+    this.home = home;
   }
 }
